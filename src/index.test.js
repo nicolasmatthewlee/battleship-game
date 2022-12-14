44526dd327c9eb05cbe4e1f3bfe8b357e50be1de
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-12-13 19:38:56 -0800

    tests for Ship.isSunk

--- src/index.test.js
+++ src/index.test.js
@@ -1,1 +1,9 @@
 import { Ship } from './index.js';
+
+test('checks if Ship(2,2,true).isSunk returns true', () => {
+  expect(new Ship(2, 2, true).isSunk()).toBe(true);
+});
+
+test('checks if Ship(3,2,false).isSunk returns false', () => {
+  expect(new Ship(3, 2, false).isSunk()).toBe(false);
+});
