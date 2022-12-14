9cc3a98ed5fb99337a2b97fb03b8d86e5cf0b281
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-12-13 19:18:31 -0800

    added Ship.hit and Ship.isSunk

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 function Ship(length, hits, sunk) {
-  return { length, hits, sunk };
+  let hit = () => {
+    this.hits++;
+  };
+
+  let isSunk = () => {
+    if (this.hits == this.length) return true;
+    else return false;
+  };
+
+  return { length, hits, sunk, hit, isSunk };
 }
 
 export { Ship };
