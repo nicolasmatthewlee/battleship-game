b7342775bfea16d5c87847affa162388b8647bcd
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-12-14 11:41:05 -0800

    setup file structure for model view controller implementation

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import { Gameboard } from './Gameboard';
 import { Ship } from './Ship';
+import { Model } from './Model';
+import { View } from './View';
+import { Controller } from './Controller';
+
+app = new Controller(Model(), View());
