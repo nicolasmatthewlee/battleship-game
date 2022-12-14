00fd54cecf4f78c960168ecf38c9197fb26a6b48
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-12-14 11:50:44 -0800

    added elements for displayStart

--- src/View.js
+++ src/View.js
@@ -1,5 +1,23 @@
+import './reset.css';
+
 class View {
-  constructor() {}
+  constructor() {
+    this.displayStart();
+  }
+
+  displayStart() {
+    const body = document.querySelector('body');
+    const titleLabel = this.createElement('div', 'title-label', 'Battleship');
+    const playButton = this.createElement('button', 'play-button', 'Play');
+    body.append(titleLabel, playButton);
+  }
+
+  createElement(type, className, content) {
+    const element = document.createElement(type);
+    element.setAttribute('class', className);
+    element.textContent = content;
+    return element;
+  }
 }
 
 export { View };
