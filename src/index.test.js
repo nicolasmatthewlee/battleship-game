53027ba5cb616f77aa5ceb3dfd99d6f17143d790
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-12-13 19:48:24 -0800

    test for Grid.grid creation

--- src/index.test.js
+++ src/index.test.js
@@ -7,3 +7,11 @@ test('checks if Ship(2,2,true).isSunk returns true', () => {
 test('checks if Ship(3,2,false).isSunk returns false', () => {
   expect(new Ship(3, 2, false).isSunk()).toBe(false);
 });
+
+test('checks if Grid(10).grid returns a 10x10 zero array', () => {
+  expect(Grid(10).grid).toBe(
+    Array(10)
+      .fill()
+      .map(() => Array(10).fill(0))
+  );
+});
