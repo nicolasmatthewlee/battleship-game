701947102d98c0998e2a039ae9ecabffcd8c5076
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-12-14 12:05:16 -0800

    added elements for displayPlaceShips

--- src/View.js
+++ src/View.js
@@ -5,6 +5,13 @@ class View {
     this.displayStart();
   }
 
+  clearDisplay() {
+    const body = document.querySelector('body');
+    while (body.firstChild) {
+      body.removeChild(body.firstChild);
+    }
+  }
+
   displayStart() {
     const body = document.querySelector('body');
     const titleLabel = this.createElement('div', 'title-label', 'Battleship');
@@ -12,7 +19,33 @@ class View {
     body.append(titleLabel, playButton);
   }
 
-  createElement(type, className, content) {
+  displayPlaceShips(shipLengths, gridLength) {
+    const body = document.querySelector('body');
+    const titleLabel = this.createElement(
+      'div',
+      'title-label',
+      'Place your ships'
+    );
+    const shipsContainer = this.createElement('div', 'ships-container');
+    for (let l of shipLengths) {
+      const ship = this.createElement('div', 'ship');
+      ship.style.setAttribute('width', l * 50 + 'px');
+      shipsContainer.append(ship);
+    }
+    const grid = this.createElement('div', 'grid');
+    for (let i = 0; i < gridLength * gridLength; i++) {
+      const gridBlock = this.createElement('div', 'grid-block');
+      grid.append(gridBlock);
+    }
+    const rotateButton = this.createElement(
+      'button',
+      'rotate-button',
+      'rotate'
+    );
+    body.append(titleLabel, shipsContainer, grid, rotateButton);
+  }
+
+  createElement(type, className, content = '') {
     const element = document.createElement(type);
     element.setAttribute('class', className);
     element.textContent = content;
